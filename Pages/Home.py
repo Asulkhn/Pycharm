444217dbc83b2b613756bde8a4e082b0dac21c6c
Asulkhn <andrishadu@gmail.com>
2024-11-14 20:04:27 +0500

    yes

---
+++ Pages/Home.py
@@ -0,0 +1,6 @@
+import streamlit as st
+class Home:
+    def __int__(self):
+        pass
+    def app(self):
+        st.write('Home')
---
+++ Pages/Project3.py
@@ -0,0 +1,6 @@
+import streamlit as st
+class Project3:
+    def __int__(self):
+        pass
+    def app(self):
+        st.write('Project3')
