0084f159411f387a0a0ea6f1f3f08c28641a7c48
Asulkhn <andrishadu@gmail.com>
2024-11-18 14:35:59 +0500

    yes

--- App.py
+++ App.py
@@ -1,24 +1,36 @@
 import streamlit as st
 from Pages import Home, Project1, Project2, Project3
+from PIL import Image
+image = Image.open('images/Th05Yin-YangOrb.png')
 from streamlit_navigation_bar import st_navbar
 
-st.set_page_config(initial_sidebar_state="collapsed")
+st.set_page_config(initial_sidebar_state="collapsed", page_icon=image)
 pages = ['Home', 'Project1', 'Project2', 'Project3']
 styles = {
     "nav": {
-        "background-color": "rgb(123, 209, 146)",
+        "background-color": "red",
+        "display": "flex",
+        "justify-content": "center"
     },
-    "div": {
-      "max-width": "32rem",
+    "img": {
+      "position": "absolute",
+      "left": "-20px",
+      "font-size": "15px",
+      "top": "4px",
+      "width": "100px",
+      "height": "40px",
     },
     "span": {
-        "border-radius": "0.5rem",
-        "color": "rgb(49, 51, 63)",
-        "margin": "0 0.125rem",
-        "padding": "0,4375rem 0.625rem",
+        "display": "block",
+        "color": "white",
+        "padding": "0,2rem 0.725rem",
+        "font-size": "14px"
     },
     "active": {
-        "background-color": "rgba(105, 114, 255, 0.25)",
+        "background-color": "white",
+        "color": "black",
+        "font-weight": "normal",
+        "padding": "14px"
     },
     "hover": {
         "background-color": "rgba(255, 255, 255, 0.35)",
